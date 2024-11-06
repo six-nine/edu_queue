9d094e5279f0c55466e65f179840baa7d9a52669
Dmitriy Sokolov <codemitry@gmail.com>
2024-11-06 19:51:38 +0300

    Added tests Formalizer project structure Added toml project file Added readme Added readme on how to run tests Added packages Fixed imports

---
+++ app/test/api/test_teacher.py
@@ -0,0 +1,12 @@
+from api.teacher import Teacher
+from unittest import TestCase
+
+class TestTeacherApi(TestCase):
+    def test_True(self):
+        assert True
+
+    def test_api_import(self):
+        assert Teacher(0, None) # Warning: Do not rely on this behaviour
+
+    def test_False(self):
+        assert False # Remove it later
